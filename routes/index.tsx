d14894ce1e64d2fa80ca206fc08341e35866f581
BinaryFly <j3roenvisser@hotmail.com>
2022-11-26 22:48:15 +0100

    added custom css and changed homepage

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,24 +1,16 @@
-import { Head } from "$fresh/runtime.ts";
+import { asset, Head } from "$fresh/runtime.ts";
 import Counter from "../islands/Counter.tsx";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Fresh App</title>
+        <title>Trying out custom css with fresh and deno</title>
+        <link rel="stylesheet" href={asset("/style.css")} />
       </Head>
-      <div>
-        <img
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the fresh logo: a sliced lemon dripping with juice"
-        />
-        <p>
-          Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
-          file, and refresh.
-        </p>
-        <Counter start={3} />
+      <div id="collapsible-one" class="collapsibleContainer">
+      </div>
+      <div id="collapsible-two" class="collapsibleContainer">
       </div>
     </>
   );
